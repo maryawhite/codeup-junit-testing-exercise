e103a44a563e1ddc6d9d66ee9601d1f5a88e7434
Mary White <mary.a.white28@gmail.com>
2021-11-15 12:04:40 -0500

    completed warmup exercise

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,6 +1,47 @@
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MyFirstTest {
+
+    @Test
+    public void testIfCompanyNameIsEquals() {
+        String expected = "Codeup";
+        String actual = "CodeUp";
+
+//        assertEquals(expected, actual); //this one fails bc they are not equal
+        assertNotEquals(expected, actual); //this one passes bc they are not equal
+    }
+
+    @Test
+    public void testIfArrayListsEquals(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
+    }
+
+    @Test
+    public void testIfArrayEquals(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers, otherNumbers);
+    }
+
+    @Test
+    public void testIfStringContains(){
+        String language = "PHP";
+
+        assertTrue("assert True", language.contains("H"));
+        assertFalse("assert False", language.contains("J"));
+    }
+
 
 }
